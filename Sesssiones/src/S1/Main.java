b1750693ab050073df89db03afe98f5e90610af6
Ricardo Villanueva Alhama <ricardo.villanueva@axisdata.net>
2024-07-24 21:22:38 +0200

    Enuciados EJ1

---
+++ Sesssiones/src/S1/Main.java
@@ -0,0 +1,45 @@
+package S1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ejercicio1();
+        ejercicio2();
+        ejercicio3();
+
+    }
+
+    private static void ejercicio1() {
+        /*
+        Escribe un programa que muestre por consola (con un print) los
+        números de 1 a 100 (ambos incluidos y con un salto de línea entre
+        cada impresión), sustituyendo los siguientes:
+        Múltiplos de 3 por la palabra "fizz".
+        Múltiplos de 5 por la palabra "buzz".
+        Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+        */
+        System.out.println("Este es el ejercicio 1 --------------------------------");
+
+    }
+
+    private static void ejercicio2() {
+        /*
+        Escribe un programa que imprima los 50 primeros números de la sucesión
+        de Fibonacci empezando en 0.
+        La serie Fibonacci se compone por una sucesión de números en
+        la que el siguiente siempre es la suma de los dos anteriores.
+        0, 1, 1, 2, 3, 5, 8, 13...
+        */
+        System.out.println("Este es el ejercicio 2 --------------------------------");
+
+    }
+
+    private static void ejercicio3() {
+        /*
+        Escribe un programa que se encargue de comprobar si un número es o no primo.
+        Hecho esto, imprime los números primos entre 1 y 100.
+        */
+        System.out.println("Este es el ejercicio 2 --------------------------------");
+
+    }
+}
